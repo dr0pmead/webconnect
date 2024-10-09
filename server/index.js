e73bd64d5f20a341ad9c9fad4d717fcc97e3e561
dropmead <nk.osinsky@icloud.com>
2024-10-09 19:46:20 +0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+// server/index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Express Server is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
